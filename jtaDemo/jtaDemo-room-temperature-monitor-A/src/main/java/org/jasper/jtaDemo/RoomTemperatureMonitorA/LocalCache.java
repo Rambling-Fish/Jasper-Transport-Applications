c81f7fd66934c1e89f93fa866485862e1fbb957a
Ray Ouellette <rouellette@coralcea.ca>
2013-10-14 13:09:59 -0400

    fix for JASPER-404
    reference DTA to provide room temperature data for notification framework

---
+++ jtaDemo/jtaDemo-room-temperature-monitor-A/src/main/java/org/jasper/jtaDemo/RoomTemperatureMonitorA/LocalCache.java
@@ -0,0 +1,46 @@
+package org.jasper.jtaDemo.RoomTemperatureMonitorA;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class LocalCache {
+	
+	
+//	private static final String ROOM_TEMP_URI = "http://coralcea.ca/jasper/roomTemperature";
+	private static final String ROOM_TEMP_URI = "http://coralcea.ca/jasper/environmentalSensor/temperature/temp";
+	private static final int DEFAULT_TEMP = 25;
+	private static RoomTemperatureData rmTempData = new RoomTemperatureData(DEFAULT_TEMP);
+	
+	private RoomTemperatureData getRoomTemperature() {
+		return rmTempData;
+	}
+		
+	private RoomTemperatureData setRoomTemperature(int currentTemp) { 
+		rmTempData.setRoomTemp(currentTemp);
+		return rmTempData;		
+	}
+	
+	public RoomTemperatureData roomTemperatureDataSetAndGet(String currentTemp){
+		int temp;
+		try{
+			String tmp = currentTemp.substring(currentTemp.lastIndexOf("/")+1);
+			temp = Integer.parseInt(tmp);
+		} catch(NumberFormatException e){
+			return getRoomTemperature();
+		}
+
+		return setRoomTemperature(temp);
+	}
+	
+	public RoomTemperatureData roomTemperatureDataSetAndGet(Map<String,Serializable>  map){
+		String currentTemp = (String) map.get(ROOM_TEMP_URI);
+		if(currentTemp == null){
+			return getRoomTemperature();
+		}else{
+			return setRoomTemperature(Integer.parseInt(currentTemp));
+		}
+	}
+	
+}
